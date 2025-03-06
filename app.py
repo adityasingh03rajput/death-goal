ce0787e7090439befa9b3d2469888725f18b2a63
adityasingh03rajput <123081746+adityasingh03rajput@users.noreply.github.com>
2025-03-06 15:20:03 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import os
-
-st.title("⚽ Web Soccer Shooter Game")
-st.write("Press the button below to launch the game!")
-
-if st.button("Start Game"):
-    os.system("python game.py")
